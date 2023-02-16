73790e92e2c8ce90b39026271d07c278cd645e75
JAndres PinillaB <andres_pb07@outlook.com>
2023-02-16 16:34:25 -0500

    se crea clase customerNotFoundException

---
+++ src/main/java/com/myjacode/springdemo/rest/CustomerNotFoundException.java
@@ -0,0 +1,26 @@
+package com.myjacode.springdemo.rest;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+	public CustomerNotFoundException() {
+	
+	}
+
+	public CustomerNotFoundException(String message) {
+	
+	}
+
+	public CustomerNotFoundException(Throwable cause) {
+		
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause) {
+		
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		
+	}
+
+}
