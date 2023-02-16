a9dd72e8ef75780bbe0c488b2b2c3fc316f355a6
JAndres PinillaB <andres_pb07@outlook.com>
2023-02-16 16:28:50 -0500

    se finaliza conf clase customerErrorResponse

--- src/main/java/com/myjacode/springdemo/rest/CustomerErrorResponse.java
+++ src/main/java/com/myjacode/springdemo/rest/CustomerErrorResponse.java
@@ -2,21 +2,48 @@ package com.myjacode.springdemo.rest;
 
 public class CustomerErrorResponse {
 	
+	
+
 	private int status;
 	private String message;
 	private Long timeStamp;
 	
-	// Constructor
+	// Constructor without fields
 	public CustomerErrorResponse() {
 		
 	}
 
+	// Constructor with fields
 	public CustomerErrorResponse(int status, String message, Long timeStamp) {
 		this.status = status;
 		this.message = message;
 		this.timeStamp = timeStamp;
 	}
 	
-	
+	// methods Getter And Setters
+	public int getStatus() {
+		return status;
+	}
 
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
 }
